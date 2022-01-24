95cb956d60614194e44932cf34a28d37f04bce3e
Roz Gillie <rgillie@uw.edu>
2022-01-23 16:27:22 -0800

    changed logo and added styling and placeholder text

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="logo"/>
+        <span className="title">ChatApp</span>
       </header>
+      <footer>
+        <input className="text-input" placeholder="Send a Message!"/> 
+        <button className="send">SEND</button>
+      </footer>
     </div>
   );
 }
